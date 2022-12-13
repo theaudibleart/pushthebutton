d8082982cb2e8b5f1b693768d43660275cff5c9b
Linda Kim <lk799@georgetown.edu>
2022-12-13 18:20:32 -0500

    removed extra from reqs

--- final_project.py
+++ final_project.py
@@ -11,7 +11,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import altair as alt
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
